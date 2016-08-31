3f26a72afc27428116d610782f6d2351dd02d02b
Jordan Plahn <jplahn@vt.edu>
2016-08-30 23:16:47 -0700

    Define city route

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/city/<city>')
+def get_city(city):
+    return city
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
